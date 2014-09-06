6e5be5db177f28798c2506ead7918513de7ac4e7
d-kostov-dev <d.kostov.dev@gmail.com>
2014-09-06 19:29:37 +0300

    Books Store Data Parser

---
+++ Exam Practice/BooksStore/BooksStoreApplication/BooksStore.ConsoleClient/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿namespace BooksStore.ConsoleClient
+{
+    using System;
+    using System.Linq;
+
+    using BooksStore.ConsoleClient.Parsers;
+    using BooksStore.Data;
+
+    public class EntryPoint
+    {
+        private static ContextFactory contextFactory;
+
+        public static void Main()
+        {
+            // Data Source=.\NEWSQL;
+            contextFactory = new ContextFactory("DefaultDbConnectionString");
+
+            // Data Source=.\SQLEXPRESS;
+            // contextFactory = new ContextFactory("ExpressDbConnectionString"); 
+
+            // Data Source=.;
+            // contextFactory = new ContextFactory("GenericDbConnectionString"); 
+
+            string fileToParse = "../../../XmlFiles/complex-books.xml";
+
+            // var booksParser = new BooksParser(contextFactory, fileToParse).ParseFile();
+        }
+    }
+}
---
+++ Exam Practice/BooksStore/BooksStoreApplication/BooksStore.ConsoleClient/Parsers/XmlDataParser.cs
@@ -0,0 +1,55 @@
+﻿namespace BooksStore.ConsoleClient.Parsers
+{
+    using System.Collections.Generic;
+    using System.Xml.Linq;
+
+    using BooksStore.ConsoleClient.Interfaces;
+    using BooksStore.Data;
+
+    public abstract class XmlDataParser : IXmlDataParser
+    {
+        protected readonly ContextFactory contextFactory;
+        protected readonly string pathToFile;
+
+        public XmlDataParser(ContextFactory factory, string fileToParse)
+        {
+            this.contextFactory = factory;
+            this.pathToFile = fileToParse;
+        }
+
+        public abstract void ParseFile();
+        
+        protected string GetSingleValue(string tag, XElement item)
+        {
+            if (item != null)
+            {
+                var result = item.Element(tag);
+
+                if (result != null)
+                {
+                    return result.Value;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Only for same tag values.
+        /// </summary>
+        protected ISet<string> GetMultipleValues(string tag, XElement itemsCollection)
+        {
+            var resultSet = new HashSet<string>();
+
+            if (itemsCollection != null)
+            {
+                foreach (var element in itemsCollection.Elements(tag))
+                {
+                    resultSet.Add(element.Value);
+                }
+            }
+
+            return resultSet;
+        }
+    }
+}
